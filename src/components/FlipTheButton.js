9d5173a7f4b7747569edb1e8006f497b82a91544
devWemillyCristhiny <devwemilly.frontend@gmail.com>
2020-11-24 15:04:58 -0300

    Terminado projeto

--- src/components/FlipTheButton.js
+++ src/components/FlipTheButton.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import './FlipTheButton.css'
 
 export default function(){
     return (
-        <div>Garrafa!!!</div>
+        <div className="container">
+            <a href="#">
+                <span className="front">Click</span>
+                <span className="center"></span>
+                <span className="back">Here</span>
+            </a>
+        </div>
     )
 }
